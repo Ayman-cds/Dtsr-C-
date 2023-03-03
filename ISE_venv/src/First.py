2341339a53154eac7b470b1a90b814673d806179
Ayman Adil <a.adil@calo.app>
2023-03-03 21:44:21 +0800

    playing around with images

--- ISE_venv/src/First.py
+++ ISE_venv/src/First.py
@@ -1,9 +1,13 @@
 import numpy as np
+import cv2
 
-a = np.array([[1, 2, 4], [3, 4, 5]])
-
-print(a)
-print(a.shape)
-rows = a.shape[0]
-cols = a.shape[1]
-print(rows, cols)
+a = cv2.imread("/home/ayman/learningStuff/ISE_venv/src/hasby.png")
+cv2.imshow("Hasby", a)
+b = cv2.cvtColor(a, cv2.COLOR_BGR2GRAY)
+b[0:50, 0:50] = 0
+cv2.imshow("Hasby in gray", b)
+cv2.waitKey(0)
+# # the third parameter "3" will tell us that the image is a 3 component image "red" "green" "blue"
+# print(a.shape)
+print(b.shape)  # this will return a color with only 2 elems
+print(b[0:5, 0:5])
